4dab6dc87ade61cc770e578d10c3db5063f7e1fd
Paulo Dias <paulovitorsouza99@gmail.com>
2023-01-27 17:51:47 -0300

    configurando navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
-import Company from './components/pages/Company'
+
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Ptojects'
 
 
 
@@ -13,26 +16,20 @@ function App() {
   return (
    <Router>
 
-      <div>
-        <Link to="/"> Home</Link>
-        <Link to="/contact"> Contato</Link>
-        <Link to="/company"> Company</Link>
-        <Link to="/new-project"> New Project</Link>
-       
-      </div>
+      <Navbar/>
      
       <Container customClass="min_height">
         <Routes>
           
               <Route path='/' element= {<Home/>}> </Route>
               <Route path='/contact' element= {<Contact/>} />
-              <Route path='/company' element= {<Company/>}> </Route>
+              <Route path='/projects' element= {<Projects/>}> </Route>
               <Route path='/new-project' element= {<NewProject/>}> </Route>
               
-        </Routes>
+        </Routes>      
        
        </Container>
-       <p> Footer</p>    
+       <Footer/>
      
    </Router>
     
